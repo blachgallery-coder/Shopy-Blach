56a9e6614200104f430c6f05a7456f909e89b20b
blachgallery-coder <blachgallery@gmail.com>
2026-06-15 19:06:59 +0200

    chore: remove debug-env API route

--- shopblach-v2/shopblach/pages/api/debug-env.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const keys = Object.keys(process.env).filter(k => 
-    k.includes('ANTHROPIC') || k.includes('CLE') || k.includes('API')
-  )
-  res.json({ keys, total: Object.keys(process.env).length })
-}
